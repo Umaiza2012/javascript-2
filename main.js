22bd6dc95faf9409ec56d8d5c5d9d7906ee245db
Umaiza20112 <hadiawaseem0302@gmail.com>
2024-12-23 16:55:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function display1()
+{
+    document.write("I am Umaiza!")
+}
+display1();
+function display(a,b)
+{
+    return a+b;
+}
+x=display(10,20)
+document.write(x);
+function myfunction (y,z)
+
+{
+    if(z=== undefined){
+        z=2;
+    }
+    document.write(y*z);
+}
